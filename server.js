79df2ec5c601d76e7c9bc2dfde6f7471272cd645
picasso <46543347+djreichel@users.noreply.github.com>
2019-06-03 14:13:59 -0400

    changed the remote db connection code again

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ var db = mongoose.connection;
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Connect to the MongoDB
 mongoose.connect(MONGODB_URI);
 
 
